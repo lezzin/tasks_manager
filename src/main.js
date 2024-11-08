4c20f2316ee624efd4a7f39e2dd2deb486be6115
Leandro Adrian da Silva <leandrinsilva22@gmail.com>
2024-11-08 15:15:18 -0300

    add font-awesome

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from './App.vue';
 import router from './router/index';
 import { createPinia } from 'pinia';
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
 import './assets/css/style.css';
+
+library.add(fas);
 
 const app = createApp({ render: () => h(App) });
 
 app.use(createPinia());
+app.component('fa', FontAwesomeIcon)
 app.use(router);
 
 app.mount('#app');
